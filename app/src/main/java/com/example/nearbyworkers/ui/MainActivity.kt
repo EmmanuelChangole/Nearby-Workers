5f7a80407a968785ff3561fcc82a39ec4e924c64
EmmanuelChangole <manuchangole@gmail.com>
2022-01-07 13:58:05 +0300

    account check for client and workers

--- app/src/main/java/com/example/nearbyworkers/ui/MainActivity.kt
+++ app/src/main/java/com/example/nearbyworkers/ui/MainActivity.kt
@@ -1,13 +1,27 @@
 package com.example.nearbyworkers.ui
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.example.nearbyworkers.R
+import com.example.nearbyworkers.model.User
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import android.content.SharedPreferences
+
+
+
 
 class MainActivity : AppCompatActivity() {
     private lateinit var auth: FirebaseAuth
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,6 +39,34 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
             finish()
         }
+        else
+        {
+            val sh = getSharedPreferences("MySharedPref", MODE_PRIVATE)
+            var account=sh.getString("account","").toString()
+
+         if(account.contentEquals("worker"))
+         {
+             var intent= Intent(this@MainActivity,WorkerActivity::class.java)
+             startActivity(intent)
+             finish()
+         }
+            else
+            {
+                var intent= Intent(this@MainActivity,ClientActivity::class.java)
+                startActivity(intent)
+                finish()
+
+            }
+
+
+
+        }
+
+
+
+
 
     }
+
+
 }
---
+++ app/src/main/java/com/example/nearbyworkers/ui/WorkerActivity.kt
@@ -0,0 +1,12 @@
+package com.example.nearbyworkers.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.nearbyworkers.R
+
+class WorkerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_worker)
+    }
+}
