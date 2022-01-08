657913975164e7b37b958bfbbab239e7cd78ed0e
EmmanuelChangole <manuchangole@gmail.com>
2022-01-08 14:16:23 +0300

    worker fragment,activity and model

---
+++ app/src/main/java/com/example/nearbyworkers/model/Worker.kt
@@ -0,0 +1,11 @@
+package com.example.nearbyworkers.model
+
+data class Worker (
+
+    var account: String = "",
+    var category:String="",
+    var description:String="",
+    var profileImageUrl: String = "",
+    var username:String=""
+
+        )
---
+++ app/src/main/java/com/example/nearbyworkers/ui/WorkViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.nearbyworkers.ui
+
+import androidx.lifecycle.ViewModel
+
+class WorkViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
